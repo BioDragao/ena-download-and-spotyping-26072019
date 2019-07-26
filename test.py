23cc8db8268c52257eafd5fe49b1d803a1ab852e
abhi18av <abhi18av@gmail.com>
2019-07-26 04:20:53 +0000

    initial commit

---
+++ test.py
@@ -0,0 +1,47 @@
+import os 
+import json 
+import shutil
+import re
+
+
+all_files = [f for f in os.listdir() if  os.path.isfile(f)]
+
+def has_fastq_in_name(string):
+    if (string.find("fastq") == -1):
+        return 0
+    else:
+        return 1
+
+all_fastq_files = list(filter(lambda x:has_fastq_in_name(x), all_files))
+
+all_fastq_names = []
+
+for a_fastq in all_fastq_files:
+    all_fastq_names.append(a_fastq.split(".")[0])
+
+#print(sorted(all_fastq_names))
+
+#print(len(all_fastq_names))
+
+all_genome_names = []
+
+for a_fastq in all_fastq_files:
+    all_genome_names.append(a_fastq.split(".")[0].split("_")[0])
+
+#print(set(all_genome_names))
+
+#regex = re.compile(r(set(all_genome_names))[0])
+#print(list(filter(regex.search, all_fastq_names)))
+
+
+def check_genome(genome_name, a_genome):
+    if genome_name == a_genome.split(".")[0].split("_")[0]:
+        #print(a_genome)
+        return True
+    
+#for a_genome in all_fastq_files:
+    #check_genome('SRR650226', a_genome)
+    #check_genome('SRR017356', a_genome)
+for a_genome in all_genome_names:
+    print("<< " + a_genome + " >>")
+    print(list(filter(lambda x: check_genome(a_genome, x), all_fastq_files)))
